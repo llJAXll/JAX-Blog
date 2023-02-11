750e71f284c827bdecfaee45f235d4cc96e97719
TOM LI <jax@JAX-MacBook-Pro.local>
2023-02-12 02:41:54 +0800

    Learn first commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-dwxv0oooobws@8ubq8o3aqu_k1%yk_6e&-3l7-2n(-yrukw2ml"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
